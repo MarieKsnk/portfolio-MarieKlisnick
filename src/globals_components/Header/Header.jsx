e8878d89cbf6d30af140697546305c659cfb7def
marieksnk <146775963+MarieKsnk@users.noreply.github.com>
2025-01-20 14:44:52 +0000

    Mise en place du header

--- src/globals_components/Header/Header.jsx
+++ src/globals_components/Header/Header.jsx
@@ -4,13 +4,26 @@ import "./Header.scss";
 
 const Header = () => {
   return (
-    <header className="header">
-      <nav>
+    <header>
+      <nav class="nav_left">
         <ul>
-          <li><Link to="/">Accueil</Link></li>
-          <li><Link to="/apropos">À propos</Link></li>
-          <li><Link to="/projets">Projets</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
+          <li>
+            <Link to="/">ACCUEIL</Link>
+          </li>
+          <li>
+            <Link to="/a-propos">A PROPOS</Link>
+          </li>
+        </ul>
+      </nav>
+      <h1 class="site-title">Marie Klisnick</h1>
+      <nav class="nav_right">
+        <ul>
+          <li>
+            <Link to="/projets">PROJETS</Link>
+          </li>
+          <li>
+            <Link to="/contact">CONTACT</Link>
+          </li>
         </ul>
       </nav>
     </header>
